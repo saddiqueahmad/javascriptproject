f02ef7ea9c4800213673e8e34a819ef3db95cabf
saddiqueahmad <106383465+saddiqueahmad@users.noreply.github.com>
2022-08-03 16:17:35 +0500

    function question 3

---
+++ javascript function task/functionquestion3.js
@@ -0,0 +1,17 @@
+function checkDriverAge(age)
+ {
+	if (Number(age) < 18)
+	 {
+		console.log("Sorry, you are too yound to drive this car. Powering off");
+	} 
+	else if (Number(age) > 18) 
+	{
+		console.log("Powering On. Enjoy the ride!");
+	}
+	else if (Number(age) === 18)
+	 {
+		console.log("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+
+checkDriverAge(92);
