dce642b6cd87b1ab17490fcbda5b761cd430444f
saddiqueahmad <106383465+saddiqueahmad@users.noreply.github.com>
2022-08-04 17:24:35 +0500

    datastructure objects

---
+++ datastructure objects/objects.js
@@ -0,0 +1,64 @@
+var user = {
+
+  name: "Siddiq",
+  age: 24,
+  hobby: "coding",
+  isMarried: false,
+  spells: ["google", "vidmate", "facebook"]
+
+};
+console.log(user);
+console.log(user.age);
+
+var array = ["Siddiq", "coding", "gaming"];
+console.log(array);
+
+var list =[
+{
+userName: "Siddiq",
+password: "secret",
+
+},
+
+{
+userName: "Ahmed",
+password: "secret",
+
+}
+];
+
+var user={
+	username : "hailey",
+	password : "hero",
+}
+
+console.log(user);
+
+var database = [
+	{
+		username : "hailey",
+		password : "zero"
+	}
+];
+
+console.log(database);
+
+
+var newsfeed = [
+	{	
+		username : "hailey",
+		timeline : "zero"
+	},
+	
+	{	
+		username : "tailey",
+		timeline : "one"
+	},
+	
+	{	
+		username : "kailey",
+		timeline : "two"
+	}	
+];
+
+console.log(newsfeed);
