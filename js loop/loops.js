bd71ceb201bc9083012e8cff0e66e5505a2f5521
saddiqueahmad <106383465+saddiqueahmad@users.noreply.github.com>
2022-08-05 17:35:01 +0500

    loops except each loop

---
+++ js loop/loops.js
@@ -0,0 +1,36 @@
+var list = [
+
+"clean room",
+"clean bath",
+"clean carpet",
+"clean yourself",
+"clean Ahmed bhai",
+
+];
+
+for (var i=0; i < list.length; i++) {
+
+	console.log(list[i] + "!");
+
+}
+
+
+/* while loop */
+
+var counterOne = 10;
+while(counterOne > 0){
+
+   console.log(counterOne);
+   counterOne--;
+
+}
+
+/* do-while loop*/
+
+var counterTwo = 10;
+do {
+
+   console.log(counterTwo);
+   counterTwo--;
+
+}while (counterTwo > 0);
