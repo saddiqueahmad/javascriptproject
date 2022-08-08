29f6d6e74ab3b461ad5dafb4ab6dbf737ca14091
saddiqueahmad <106383465+saddiqueahmad@users.noreply.github.com>
2022-08-08 21:40:37 +0500

    complete exercise scope

--- js scope/scope.js
+++ js scope/scope.js
@@ -3,6 +3,39 @@ function q1() {
     if(a > 1) {
         a = 3;
     }
-    document.write("value of an alert is = "+a);
+    document.write("value of an alert is = "+a+ "<br>");
 }
+
 q1();
+
+// question 3;
+
+function q3() {
+    window.a = "hello";
+}
+
+function q32() {
+    alert(a);
+}
+q32();
+
+// question 4;
+
+var a = 1;
+function q4() {
+    var a = "test";
+    alert(a);
+}
+q4();
+
+// question 5
+
+var a = 2;
+if (true) {
+    var a = 5;
+    alert(a);
+}
+alert(a);
+
+ans:two alert boxes appearred with value= 5 because inside the block we assign a=5.
+And outside the block we assign a as value=2.
